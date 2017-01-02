12d7ba71311c01d8f2636ecf9d4ae57cf3cf8d4c
wayne <lightww@me.com>
2017-01-02 08:38:04 +0800

    add posts nested route

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :group_id, :user_id)
+
+  end
+
+
+end
